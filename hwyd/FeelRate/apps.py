d250f52bf76d6636acf5b762346fbdc7797c70b1
ReZa QaVi <reza.qavi@tapsi.cab>
2023-06-04 17:33:37 +0330

    Delete feelRate app

--- hwyd/FeelRate/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class FeelrateConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'FeelRate'
--- hwyd/FeelRate/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-from ..members.models import Member
-# Create your models here.
-class FeelRate(models.Model):
-  member = models.ForeignKey(Member, on_delete=models.DO_NOTHING)
-  date = models.DateField()
-  rate = models.IntegerField()
-  
